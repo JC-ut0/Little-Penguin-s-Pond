ef3150ef4ed17222e7f3ed6ac5635e2b27151bff
Jesse Xu <yexiong.xu@mail.utoronto.ca>
2019-12-26 04:14:52 -0500

    Implement the animation of walking and eye closing

---
+++ Little Penguin's Pond/Assets/Script/CharactorAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactorAnimator : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = .1f;
+
+    float speedPrecent;
+
+    Animator animator;
+    PlayerMovement movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedPrecent = movement.horizontalMove * movement.horizontalMove;
+        animator.SetFloat("SpeedPercent", speedPrecent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
---
+++ Little Penguin's Pond/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float horizontalMove = 0f;
+    public float verticalMove = 0f;
+    public float runSpeed = 40f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove, false, false);
+    }
+}
