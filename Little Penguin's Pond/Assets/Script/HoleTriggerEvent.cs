72ce69af14d532b139343acb7580d90a2fbb8172
Jesse Xu <yexiong.xu@mail.utoronto.ca>
2019-12-27 03:45:03 -0500

    Add new Hotel Scene

--- Little Penguin's Pond/Assets/Script/HoleTriggerEvent.cs
+++ Little Penguin's Pond/Assets/Script/HoleTriggerEvent.cs
@@ -6,10 +6,20 @@ using UnityEngine.SceneManagement;
 public class HoleTriggerEvent : MonoBehaviour
 {
     public string scene;
+    public bool enterTrigger;
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        if(collider.tag == "Player")
+        if(collider.tag == "Player" && !enterTrigger)
+        {
+            Debug.Log("Load Next Scene");
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player" && enterTrigger)
         {
             Debug.Log("Load Next Scene");
             SceneManager.LoadScene(scene);
