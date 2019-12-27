37e3e85d227ab1069c495a9f13df21b5052cc914
Jesse Xu <yexiong.xu@mail.utoronto.ca>
2019-12-27 04:43:30 -0500

    Add JoyStick to all Scenes

--- Little Penguin's Pond/Assets/Script/PlayerMovement.cs
+++ Little Penguin's Pond/Assets/Script/PlayerMovement.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,27 +8,45 @@ public class PlayerMovement : MonoBehaviour
     public float verticalMove = 0f;
     public float runSpeed = 40f;
     public bool jump = false;
+    public Joystick joystick;
 
+    private void Start()
+    {
+        if (FindObjectOfType<Joystick>() != null)
+        {
+            joystick = FindObjectOfType<Joystick>();
+        }
+    }
 
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
-        if (Input.GetKeyDown(KeyCode.W))
+        if (joystick != null)
+        {
+            if (joystick.Horizontal > .2f)
+            {
+                horizontalMove = runSpeed;
+            }
+            else if (joystick.Horizontal < -.2f)
+            {
+                horizontalMove = -runSpeed;
+            }
+            else
+            {
+                horizontalMove = 0f;
+            }
+            verticalMove = joystick.Vertical * runSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.W) || joystick.Vertical >= 0.4f)
         {
             jump = true;
         }
-
-
-
     }
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
-
     }
 }
