8600fe4ba9eb451537f2a1fb54277e95beb01600
Jesse Xu <yexiong.xu@mail.utoronto.ca>
2019-12-28 23:12:42 -0500

    Update UI

--- Little Penguin's Pond/Assets/Script/UIManager.cs
+++ Little Penguin's Pond/Assets/Script/UIManager.cs
@@ -1,24 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject BGM;
     public GameObject shopUI;
     public GameObject inventoryUI;
+    public GameObject musicOff;
+    public GameObject musicOn;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void OnShopOpen()
     {
         shopUI.SetActive(true);
@@ -34,5 +27,17 @@ public class UIManager : MonoBehaviour
     public void OnInventoryClosed()
     {
         inventoryUI.SetActive(false);
+    }
+    public void OnMute()
+    {
+        BGM.SetActive(false);
+        musicOff.SetActive(true);
+        musicOn.SetActive(false);
+    }
+    public void OnMusic()
+    {
+        BGM.SetActive(true);
+        musicOff.SetActive(false);
+        musicOn.SetActive(true);
     }
 }
