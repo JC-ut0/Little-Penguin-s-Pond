9b9c6c21d01ee161ccfd60c533521f593bffa3d1
Jesse Xu <yexiong.xu@mail.utoronto.ca>
2019-12-28 22:15:09 -0500

    Import Icon Set and Update UI

---
+++ Little Penguin's Pond/Assets/Script/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject shopUI;
+    public GameObject inventoryUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnShopOpen()
+    {
+        shopUI.SetActive(true);
+    }
+    public void OnShopClosed()
+    {
+        shopUI.SetActive(false);
+    }
+    public void OnInventoryOpen()
+    {
+        inventoryUI.SetActive(true);
+    }
+    public void OnInventoryClosed()
+    {
+        inventoryUI.SetActive(false);
+    }
+}
