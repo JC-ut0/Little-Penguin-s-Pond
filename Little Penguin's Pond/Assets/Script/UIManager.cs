085c592a29e00802ecba072835fa0269753575f7
Jesse Xu <yexiong.xu@mail.utoronto.ca>
2019-12-30 01:57:37 -0500

    Update BGM

--- Little Penguin's Pond/Assets/Script/UIManager.cs
+++ Little Penguin's Pond/Assets/Script/UIManager.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
@@ -11,29 +8,42 @@ public class UIManager : MonoBehaviour
     public GameObject musicOff;
     public GameObject musicOn;
 
+    private void Start()
+    {
+        // if BGM exists
+        if (GameObject.FindGameObjectWithTag("BGM") != null)
+        {
+            BGM = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>();
+        }
+    }
 
     public void OnShopOpen()
     {
         shopUI.SetActive(true);
     }
+
     public void OnShopClosed()
     {
         shopUI.SetActive(false);
     }
+
     public void OnInventoryOpen()
     {
         inventoryUI.SetActive(true);
     }
+
     public void OnInventoryClosed()
     {
         inventoryUI.SetActive(false);
     }
+
     public void OnMute()
     {
         BGM.mute = true;
         musicOff.SetActive(true);
         musicOn.SetActive(false);
     }
+
     public void OnMusic()
     {
         BGM.mute = false;
